237a1dbf43b0b1d69a85503fd5c2096fab8b41c5
movi1 <98534830+movi1@users.noreply.github.com>
2022-11-11 11:01:44 +0000

    update the useState

---
+++ src/components/ArriveDropdown.js
@@ -0,0 +1,32 @@
+import Form from 'react-bootstrap/Form';
+
+function ArriveDropdown(props) {
+
+  const { setArrival, stations } = props;
+
+  const handleChange = event => {
+    console.log(event.target.value);
+    setArrival(event.target.value);
+  };
+
+
+  return (
+    <>
+      <Form.Select aria-label="Select your Destination Station" name="arrive-stations-select" onChange={handleChange}>
+
+        <option>Select your Destination Station</option>
+        {
+          stations.map((station, i) => {
+            return (
+              <option value={station.name} key={i} href={'#/ + station.name'}>{station.name}</option>
+            )
+          })
+        };
+      </Form.Select>
+    </>
+
+
+  );
+}
+
+export default ArriveDropdown;
---
+++ src/components/DepartureDropdown.js
@@ -0,0 +1,37 @@
+import Form from 'react-bootstrap/Form';
+
+function DepartureDropdown(props) {
+
+    const { setDeparture, stations } = props;
+    // const { stations } = props;
+
+    const handleChange = event => {
+        console.log(event.target.value);
+        setDeparture(event.target.value);
+    };
+
+    // console.log(stations.stations)
+    return (
+
+
+        <>
+
+            <Form.Select aria-label="Select your Departure Station" name="departure-stations-select" onChange={handleChange}>
+
+                <option>Select your Departure Station</option>
+                {
+                    stations.map((station, i) => {
+                        return (
+                            <option value={station.name} key={i} href={'#/ + station.name'}>{station.name}</option>
+                        )
+                    })
+                };
+            </Form.Select>
+
+        </>
+
+
+    );
+
+}
+export default DepartureDropdown;
